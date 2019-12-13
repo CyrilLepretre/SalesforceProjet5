aeee1b110e01617dccfba05f3b432b10f8dbef42
CyrilLepretre <cyril.lepretre@axa.fr>
2019-12-13 23:56:04 +0100

    First integration test added

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,6 +4,7 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,6 +55,14 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        String vehicleRegistrationNumber = null;
+        try {
+            vehicleRegistrationNumber = inputReaderUtil.readVehicleRegistrationNumber();
+        } catch (Exception e) {
+            System.out.println("Error while getting vehicle registration number");
+        }
+        Ticket shouldBeSavedTicket = ticketDAO.getTicket(vehicleRegistrationNumber);
+        assertEquals(shouldBeSavedTicket.getVehicleRegNumber(), "ABCDEF");
     }
 
     @Test
