0e750e3d6aa32c7222571c5f8b8c80a8075b1fda
CyrilLepretre <cyril.lepretre@axa.fr>
2019-12-13 16:13:05 +0100

    TODO#1 code fixed to make unit tests pass

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -9,12 +9,13 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
+        // Usage of getTime to get inHour and outHour milliseconds, and compare them
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        // Difference between outHour and inHour in milliseconds is a long, we use a double to store it transformed in hours
+        // Must divide by double and not integer to get a double result
+        double duration = (outHour - inHour)/1000.0/60.0/60.0;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
