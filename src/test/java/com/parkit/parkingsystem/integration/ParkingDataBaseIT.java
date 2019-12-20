0c015af3d0337b27a6321ac42fbc877c25a9da60
CyrilLepretre <cyril.lepretre@axa.fr>
2019-12-20 17:51:43 +0100

    TODO System Integration Tests added

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -15,7 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,25 +53,40 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+        int spotBefore = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR); // get available spot before incoming vehicle
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-        String vehicleRegistrationNumber = null;
-        try {
-            vehicleRegistrationNumber = inputReaderUtil.readVehicleRegistrationNumber();
-        } catch (Exception e) {
-            System.out.println("Error while getting vehicle registration number");
-        }
-        Ticket shouldBeSavedTicket = ticketDAO.getTicket(vehicleRegistrationNumber);
-        assertEquals(shouldBeSavedTicket.getVehicleRegNumber(), "ABCDEF");
+        int spotAfter = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR); // get available spot after incoming vehicle
+        assertNotEquals(spotBefore, spotAfter);
+        //Get a ticket from the DB with the vehicle Registration Number previously added with processIncomingVehicle()
+        Ticket ticketFromDB = ticketDAO.getTicket(this.getVehicleRegistrationNumber(inputReaderUtil));
+        assertEquals(ticketFromDB.getVehicleRegNumber(), "ABCDEF");
     }
 
     @Test
     public void testParkingLotExit(){
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        // Use of a sleep to force the outTime to be after the inTime in database
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket ticketFromDB = ticketDAO.getTicket(this.getVehicleRegistrationNumber(inputReaderUtil));
+        assertNotEquals(ticketFromDB.getPrice(), 0);
+        assertTrue(ticketFromDB.getInTime().before(ticketFromDB.getOutTime()));
+    }
+
+    private String getVehicleRegistrationNumber(InputReaderUtil inputReaderUtil){
+        String vehicleRegistrationNumber = null;
+        try {
+            vehicleRegistrationNumber = inputReaderUtil.readVehicleRegistrationNumber();
+        } catch (Exception e) {
+            System.out.println("Error while getting vehicle registration number");
+        }
+        return vehicleRegistrationNumber;
     }
 
 }
