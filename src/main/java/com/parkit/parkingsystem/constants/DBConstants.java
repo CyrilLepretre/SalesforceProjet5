964094b6a9c379c79b41564ed496ab2ef1d88cb0
CyrilLepretre <cyril.lepretre@axa.fr>
2020-01-17 15:52:36 +0100

    Add constants to host the discount rate by vehicle type

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -9,5 +9,6 @@ public class DBConstants {
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
     public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
 
-    public static final String CHECK_RECURRING_USER = "select count(*) from ticket where VEHICLE_REG_NUMBER = ?";
+    // We don't add a new column in ticket table and request with OUT_TIME is not null to verify if the customer has alrady come in the parking
+    public static final String CHECK_RECURRING_USER = "select count(*) from ticket where OUT_TIME is not null and VEHICLE_REG_NUMBER = ?";
 }
