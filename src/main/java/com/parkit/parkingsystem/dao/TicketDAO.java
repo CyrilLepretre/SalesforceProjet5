0d10707f7278ecada3c2f8d480f30db80b2d93fa
CyrilLepretre <cyril.lepretre@axa.fr>
2020-01-31 14:04:41 +0100

    Checkstyle rules applied 1 missing abbreviation

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -5,13 +5,12 @@ import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TicketDAO {
 
@@ -19,7 +18,13 @@ public class TicketDAO {
 
   public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-  public boolean saveTicket(Ticket ticket){
+  /**
+   * Save a ticket in DataBase.
+   *
+   * @param ticket to save in DataBase
+   * @return true if the ticket was correctly saved, false else
+   */
+  public boolean saveTicket(Ticket ticket) {
     Connection con = null;
     try {
       con = dataBaseConfig.getConnection();
@@ -30,16 +35,23 @@ public class TicketDAO {
       ps.setString(2, ticket.getVehicleRegNumber());
       ps.setDouble(3, ticket.getPrice());
       ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
-      ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())) );
+      ps.setTimestamp(5,(ticket.getOutTime() == null) ? null :
+              (new Timestamp(ticket.getOutTime().getTime())));
       return ps.execute();
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error fetching next available slot",ex);
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
       return false;
     }
   }
 
+  /**
+   * Get the ticket from DataBase of a vehicle.
+   *
+   * @param vehicleRegNumber corresponding to the vehicle number
+   * @return the ticket of the vehicle passed as paramater
+   */
   public Ticket getTicket(String vehicleRegNumber) {
     Connection con = null;
     Ticket ticket = null;
@@ -49,9 +61,10 @@ public class TicketDAO {
       //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
       ps.setString(1,vehicleRegNumber);
       ResultSet rs = ps.executeQuery();
-      if(rs.next()){
+      if (rs.next()) {
         ticket = new Ticket();
-        ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),false);
+        ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1),
+                ParkingType.valueOf(rs.getString(6)),false);
         ticket.setParkingSpot(parkingSpot);
         ticket.setId(rs.getInt(2));
         ticket.setVehicleRegNumber(vehicleRegNumber);
@@ -61,14 +74,20 @@ public class TicketDAO {
       }
       dataBaseConfig.closeResultSet(rs);
       dataBaseConfig.closePreparedStatement(ps);
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error fetching next available slot",ex);
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
       return ticket;
     }
   }
 
+  /**
+   * Update a ticket on DataBase.
+   *
+   * @param ticket to update
+   * @return true if the ticket was correctly updated, false else
+   */
   public boolean updateTicket(Ticket ticket) {
     Connection con = null;
     try {
@@ -79,22 +98,22 @@ public class TicketDAO {
       ps.setInt(3,ticket.getId());
       ps.execute();
       return true;
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error saving ticket info",ex);
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
     }
     return false;
   }
 
   /**
-   * Indicates if a vehicle has already come in the parking, and so if the user is a recurring one
-   * Added for the discount feature
+   * Indicates if a vehicle has already come in the parking, and so if the user is a recurring one.
+   * Added for the discount feature.
    *
    * @param vehicleRegNumber the vehicle Reg Number
    * @return true if the vehicle has already come, false else
    */
-  public boolean isRecurringUser(String vehicleRegNumber){
+  public boolean isRecurringUser(String vehicleRegNumber) {
     Connection con = null;
     int numberTicketsVehicle = 0;
     try {
@@ -102,14 +121,14 @@ public class TicketDAO {
       PreparedStatement ps = con.prepareStatement(DBConstants.CHECK_RECURRING_USER);
       ps.setString(1,vehicleRegNumber);
       ResultSet rs = ps.executeQuery();
-      if(rs.next()){
+      if (rs.next()) {
         numberTicketsVehicle = rs.getInt(1);
       }
       dataBaseConfig.closeResultSet(rs);
       dataBaseConfig.closePreparedStatement(ps);
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error checking recurrent user",ex);
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
     }
     return (numberTicketsVehicle > 0);
