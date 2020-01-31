b08a00e819a8e50252c2d1deccbc3465348222be
CyrilLepretre <cyril.lepretre@axa.fr>
2020-01-31 13:50:10 +0100

    Checkstyle rules applied 1 missing abbreviation

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -4,40 +4,49 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ParkingSpotDAO {
   private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
 
   public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-  public int getNextAvailableSlot(ParkingType parkingType){
+  /**
+   * Get the next available spot.
+   * @param parkingType indicates what kind of parking we look for
+   * @return the number of available spot
+   */
+  public int getNextAvailableSlot(ParkingType parkingType) {
     Connection con = null;
-    int result=-1;
+    int result = -1;
     try {
       con = dataBaseConfig.getConnection();
       PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
       ps.setString(1, parkingType.toString());
       ResultSet rs = ps.executeQuery();
-      if(rs.next()){
+      if (rs.next()) {
         result = rs.getInt(1);;
       }
       dataBaseConfig.closeResultSet(rs);
       dataBaseConfig.closePreparedStatement(ps);
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error fetching next available slot",ex);
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
     }
     return result;
   }
 
-  public boolean updateParking(ParkingSpot parkingSpot){
+  /**
+   * Updates a parking.
+   * @param parkingSpot to update the parking
+   * @return true if the parking as been updated, false else
+   */
+  public boolean updateParking(ParkingSpot parkingSpot) {
     //update the availability fo that parking slot
     Connection con = null;
     try {
@@ -48,10 +57,10 @@ public class ParkingSpotDAO {
       int updateRowCount = ps.executeUpdate();
       dataBaseConfig.closePreparedStatement(ps);
       return (updateRowCount == 1);
-    }catch (Exception ex){
+    } catch (Exception ex) {
       logger.error("Error updating parking info",ex);
       return false;
-    }finally {
+    } finally {
       dataBaseConfig.closeConnection(con);
     }
   }
