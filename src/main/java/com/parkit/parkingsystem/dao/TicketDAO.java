4ccf7d5907b5d36df590bdd8da5a80f0c0f8dda3
CyrilLepretre <cyril.lepretre@axa.fr>
2020-01-31 11:34:09 +0100

    Checkstyle - indentation with 2 spaces

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -15,103 +15,103 @@ import java.sql.Timestamp;
 
 public class TicketDAO {
 
-    private static final Logger logger = LogManager.getLogger("TicketDAO");
+  private static final Logger logger = LogManager.getLogger("TicketDAO");
 
-    public DataBaseConfig dataBaseConfig = new DataBaseConfig();
+  public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public boolean saveTicket(Ticket ticket){
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.SAVE_TICKET);
-            //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
-            //ps.setInt(1,ticket.getId());
-            ps.setInt(1,ticket.getParkingSpot().getId());
-            ps.setString(2, ticket.getVehicleRegNumber());
-            ps.setDouble(3, ticket.getPrice());
-            ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
-            ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())) );
-            return ps.execute();
-        }catch (Exception ex){
-            logger.error("Error fetching next available slot",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-            return false;
-        }
+  public boolean saveTicket(Ticket ticket){
+    Connection con = null;
+    try {
+      con = dataBaseConfig.getConnection();
+      PreparedStatement ps = con.prepareStatement(DBConstants.SAVE_TICKET);
+      //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
+      //ps.setInt(1,ticket.getId());
+      ps.setInt(1,ticket.getParkingSpot().getId());
+      ps.setString(2, ticket.getVehicleRegNumber());
+      ps.setDouble(3, ticket.getPrice());
+      ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
+      ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())) );
+      return ps.execute();
+    }catch (Exception ex){
+      logger.error("Error fetching next available slot",ex);
+    }finally {
+      dataBaseConfig.closeConnection(con);
+      return false;
     }
+  }
 
-    public Ticket getTicket(String vehicleRegNumber) {
-        Connection con = null;
-        Ticket ticket = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_TICKET);
-            //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
-            ps.setString(1,vehicleRegNumber);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                ticket = new Ticket();
-                ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),false);
-                ticket.setParkingSpot(parkingSpot);
-                ticket.setId(rs.getInt(2));
-                ticket.setVehicleRegNumber(vehicleRegNumber);
-                ticket.setPrice(rs.getDouble(3));
-                ticket.setInTime(rs.getTimestamp(4));
-                ticket.setOutTime(rs.getTimestamp(5));
-            }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
-            logger.error("Error fetching next available slot",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-            return ticket;
-        }
+  public Ticket getTicket(String vehicleRegNumber) {
+    Connection con = null;
+    Ticket ticket = null;
+    try {
+      con = dataBaseConfig.getConnection();
+      PreparedStatement ps = con.prepareStatement(DBConstants.GET_TICKET);
+      //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
+      ps.setString(1,vehicleRegNumber);
+      ResultSet rs = ps.executeQuery();
+      if(rs.next()){
+        ticket = new Ticket();
+        ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),false);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setId(rs.getInt(2));
+        ticket.setVehicleRegNumber(vehicleRegNumber);
+        ticket.setPrice(rs.getDouble(3));
+        ticket.setInTime(rs.getTimestamp(4));
+        ticket.setOutTime(rs.getTimestamp(5));
+      }
+      dataBaseConfig.closeResultSet(rs);
+      dataBaseConfig.closePreparedStatement(ps);
+    }catch (Exception ex){
+      logger.error("Error fetching next available slot",ex);
+    }finally {
+      dataBaseConfig.closeConnection(con);
+      return ticket;
     }
+  }
 
-    public boolean updateTicket(Ticket ticket) {
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
-            ps.setDouble(1, ticket.getPrice());
-            ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
-            ps.setInt(3,ticket.getId());
-            ps.execute();
-            return true;
-        }catch (Exception ex){
-            logger.error("Error saving ticket info",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-        return false;
+  public boolean updateTicket(Ticket ticket) {
+    Connection con = null;
+    try {
+      con = dataBaseConfig.getConnection();
+      PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
+      ps.setDouble(1, ticket.getPrice());
+      ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
+      ps.setInt(3,ticket.getId());
+      ps.execute();
+      return true;
+    }catch (Exception ex){
+      logger.error("Error saving ticket info",ex);
+    }finally {
+      dataBaseConfig.closeConnection(con);
     }
+    return false;
+  }
 
-    /**
-     * Indicates if a vehicle has already come in the parking, and so if the user is a recurring one
-     * Added for the discount feature
-     *
-     * @param vehicleRegNumber the vehicle Reg Number
-     * @return true if the vehicle has already come, false else
-     */
-    public boolean isRecurringUser(String vehicleRegNumber){
-        Connection con = null;
-        int numberTicketsVehicle = 0;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.CHECK_RECURRING_USER);
-            ps.setString(1,vehicleRegNumber);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                numberTicketsVehicle = rs.getInt(1);
-            }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
-            logger.error("Error checking recurrent user",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-        return (numberTicketsVehicle > 0);
+  /**
+   * Indicates if a vehicle has already come in the parking, and so if the user is a recurring one
+   * Added for the discount feature
+   *
+   * @param vehicleRegNumber the vehicle Reg Number
+   * @return true if the vehicle has already come, false else
+   */
+  public boolean isRecurringUser(String vehicleRegNumber){
+    Connection con = null;
+    int numberTicketsVehicle = 0;
+    try {
+      con = dataBaseConfig.getConnection();
+      PreparedStatement ps = con.prepareStatement(DBConstants.CHECK_RECURRING_USER);
+      ps.setString(1,vehicleRegNumber);
+      ResultSet rs = ps.executeQuery();
+      if(rs.next()){
+        numberTicketsVehicle = rs.getInt(1);
+      }
+      dataBaseConfig.closeResultSet(rs);
+      dataBaseConfig.closePreparedStatement(ps);
+    }catch (Exception ex){
+      logger.error("Error checking recurrent user",ex);
+    }finally {
+      dataBaseConfig.closeConnection(con);
     }
+    return (numberTicketsVehicle > 0);
+  }
 }
--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -14,145 +14,145 @@ import java.util.Date;
 
 public class ParkingService {
 
-    private static final Logger logger = LogManager.getLogger("ParkingService");
+  private static final Logger logger = LogManager.getLogger("ParkingService");
 
-    private static FareCalculatorService fareCalculatorService = new FareCalculatorService();
+  private static FareCalculatorService fareCalculatorService = new FareCalculatorService();
 
-    private InputReaderUtil inputReaderUtil;
-    private ParkingSpotDAO parkingSpotDAO;
-    private  TicketDAO ticketDAO;
+  private InputReaderUtil inputReaderUtil;
+  private ParkingSpotDAO parkingSpotDAO;
+  private  TicketDAO ticketDAO;
 
-    public ParkingService(InputReaderUtil inputReaderUtil, ParkingSpotDAO parkingSpotDAO, TicketDAO ticketDAO){
-        this.inputReaderUtil = inputReaderUtil;
-        this.parkingSpotDAO = parkingSpotDAO;
-        this.ticketDAO = ticketDAO;
+  public ParkingService(InputReaderUtil inputReaderUtil, ParkingSpotDAO parkingSpotDAO, TicketDAO ticketDAO){
+    this.inputReaderUtil = inputReaderUtil;
+    this.parkingSpotDAO = parkingSpotDAO;
+    this.ticketDAO = ticketDAO;
+  }
+
+  /**
+   * Process of incoming vehicles
+   * Feature added on 01/10/2020 : controls if it's a reccurent user in order to display a message for a discount
+   *                               based on a request wich verify if there is already a ticket in database with OUT_TIME not null
+   */
+
+  public void processIncomingVehicle() {
+    try{
+      ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
+      if(parkingSpot !=null && parkingSpot.getId() > 0){
+        String vehicleRegNumber = getVehichleRegNumber();
+        Ticket ticket = new Ticket();
+        this.discountProcess(parkingSpot.getParkingType(), ticket, true, ticketDAO.isRecurringUser(vehicleRegNumber));
+        parkingSpot.setAvailable(false);
+        parkingSpotDAO.updateParking(parkingSpot);//allot this parking space and mark it's availability as false
+
+        Date inTime = new Date();
+        //ticket.setId(ticketID);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber(vehicleRegNumber);
+        ticket.setPrice(0);
+        ticket.setInTime(inTime);
+        ticket.setOutTime(null);
+        ticketDAO.saveTicket(ticket);
+        System.out.println("Generated Ticket and saved in DB");
+        System.out.println("Please park your vehicle in spot number:"+parkingSpot.getId());
+        System.out.println("Recorded in-time for vehicle number:"+vehicleRegNumber+" is:"+inTime);
+      }
+    }catch(Exception e){
+      logger.error("Unable to process incoming vehicle",e);
     }
+  }
 
-    /**
-     * Process of incoming vehicles
-     * Feature added on 01/10/2020 : controls if it's a reccurent user in order to display a message for a discount
-     *                               based on a request wich verify if there is already a ticket in database with OUT_TIME not null
-     */
-
-    public void processIncomingVehicle() {
-        try{
-            ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
-            if(parkingSpot !=null && parkingSpot.getId() > 0){
-                String vehicleRegNumber = getVehichleRegNumber();
-                Ticket ticket = new Ticket();
-                this.discountProcess(parkingSpot.getParkingType(), ticket, true, ticketDAO.isRecurringUser(vehicleRegNumber));
-                parkingSpot.setAvailable(false);
-                parkingSpotDAO.updateParking(parkingSpot);//allot this parking space and mark it's availability as false
-
-                Date inTime = new Date();
-                //ticket.setId(ticketID);
-                ticket.setParkingSpot(parkingSpot);
-                ticket.setVehicleRegNumber(vehicleRegNumber);
-                ticket.setPrice(0);
-                ticket.setInTime(inTime);
-                ticket.setOutTime(null);
-                ticketDAO.saveTicket(ticket);
-                System.out.println("Generated Ticket and saved in DB");
-                System.out.println("Please park your vehicle in spot number:"+parkingSpot.getId());
-                System.out.println("Recorded in-time for vehicle number:"+vehicleRegNumber+" is:"+inTime);
-            }
-        }catch(Exception e){
-            logger.error("Unable to process incoming vehicle",e);
-        }
+  /**
+   * Sets the discount value for recurring users and prints a specif message for incoming vehicles
+   *
+   */
+  private void discountProcess(ParkingType type, Ticket ticket, boolean incoming, boolean recurrentUser){
+    if (recurrentUser) {
+      switch (type) {
+        case CAR:
+          if (incoming) {
+            System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a " + (int) (Fare.CAR_DISCOUNT * 100) + "% discount.");
+          }
+          ticket.setDiscount(Fare.CAR_DISCOUNT);
+          break;
+        case BIKE:
+          if (incoming) {
+            System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a " + (int) (Fare.BIKE_DISCOUNT * 100) + "% discount.");
+          }
+          ticket.setDiscount(Fare.BIKE_DISCOUNT);
+          break;
+      }
+    }else{
+      ticket.setDiscount(0.0);
     }
+  }
 
-    /**
-     * Sets the discount value for recurring users and prints a specif message for incoming vehicles
-     *
-     */
-    private void discountProcess(ParkingType type, Ticket ticket, boolean incoming, boolean recurrentUser){
-        if (recurrentUser) {
-            switch (type) {
-                case CAR:
-                    if (incoming) {
-                        System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a " + (int) (Fare.CAR_DISCOUNT * 100) + "% discount.");
-                    }
-                    ticket.setDiscount(Fare.CAR_DISCOUNT);
-                    break;
-                case BIKE:
-                    if (incoming) {
-                        System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a " + (int) (Fare.BIKE_DISCOUNT * 100) + "% discount.");
-                    }
-                    ticket.setDiscount(Fare.BIKE_DISCOUNT);
-                    break;
-            }
-        }else{
-            ticket.setDiscount(0.0);
-        }
+  private String getVehichleRegNumber() throws Exception {
+    System.out.println("Please type the vehicle registration number and press enter key");
+    return inputReaderUtil.readVehicleRegistrationNumber();
+  }
+
+  public ParkingSpot getNextParkingNumberIfAvailable(){
+    int parkingNumber=0;
+    ParkingSpot parkingSpot = null;
+    try{
+      ParkingType parkingType = getVehichleType();
+      parkingNumber = parkingSpotDAO.getNextAvailableSlot(parkingType);
+      if(parkingNumber > 0){
+        parkingSpot = new ParkingSpot(parkingNumber,parkingType, true);
+      }else{
+        throw new Exception("Error fetching parking number from DB. Parking slots might be full");
+      }
+    }catch(IllegalArgumentException ie){
+      logger.error("Error parsing user input for type of vehicle", ie);
+    }catch(Exception e){
+      logger.error("Error fetching next available parking slot", e);
     }
+    return parkingSpot;
+  }
 
-    private String getVehichleRegNumber() throws Exception {
-        System.out.println("Please type the vehicle registration number and press enter key");
-        return inputReaderUtil.readVehicleRegistrationNumber();
+  private ParkingType getVehichleType(){
+    System.out.println("Please select vehicle type from menu");
+    System.out.println("1 CAR");
+    System.out.println("2 BIKE");
+    int input = inputReaderUtil.readSelection();
+    switch(input){
+      case 1: {
+        return ParkingType.CAR;
+      }
+      case 2: {
+        return ParkingType.BIKE;
+      }
+      default: {
+        System.out.println("Incorrect input provided");
+        throw new IllegalArgumentException("Entered input is invalid");
+      }
     }
+  }
 
-    public ParkingSpot getNextParkingNumberIfAvailable(){
-        int parkingNumber=0;
-        ParkingSpot parkingSpot = null;
-        try{
-            ParkingType parkingType = getVehichleType();
-            parkingNumber = parkingSpotDAO.getNextAvailableSlot(parkingType);
-            if(parkingNumber > 0){
-                parkingSpot = new ParkingSpot(parkingNumber,parkingType, true);
-            }else{
-                throw new Exception("Error fetching parking number from DB. Parking slots might be full");
-            }
-        }catch(IllegalArgumentException ie){
-            logger.error("Error parsing user input for type of vehicle", ie);
-        }catch(Exception e){
-            logger.error("Error fetching next available parking slot", e);
-        }
-        return parkingSpot;
+  /**
+   * Process of exiting vehicles
+   * Feature added on 01/10/2020 : controls if it's a reccurent user in order to apply a discout, 0 else
+   */
+
+  public void processExitingVehicle() {
+    try{
+      String vehicleRegNumber = getVehichleRegNumber();
+      Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
+      ParkingSpot parkingSpot = ticket.getParkingSpot();
+      this.discountProcess(parkingSpot.getParkingType(), ticket, false, ticketDAO.isRecurringUser(vehicleRegNumber));
+      Date outTime = new Date();
+      ticket.setOutTime(outTime);
+      fareCalculatorService.calculateFare(ticket);
+      if(ticketDAO.updateTicket(ticket)) {
+        parkingSpot.setAvailable(true);
+        parkingSpotDAO.updateParking(parkingSpot);
+        System.out.println("Please pay the parking fare:" + ticket.getPrice());
+        System.out.println("Recorded out-time for vehicle number:" + ticket.getVehicleRegNumber() + " is:" + outTime);
+      }else{
+        System.out.println("Unable to update ticket information. Error occurred");
+      }
+    }catch(Exception e){
+      logger.error("Unable to process exiting vehicle",e);
     }
-
-    private ParkingType getVehichleType(){
-        System.out.println("Please select vehicle type from menu");
-        System.out.println("1 CAR");
-        System.out.println("2 BIKE");
-        int input = inputReaderUtil.readSelection();
-        switch(input){
-            case 1: {
-                return ParkingType.CAR;
-            }
-            case 2: {
-                return ParkingType.BIKE;
-            }
-            default: {
-                System.out.println("Incorrect input provided");
-                throw new IllegalArgumentException("Entered input is invalid");
-            }
-        }
-    }
-
-    /**
-     * Process of exiting vehicles
-     * Feature added on 01/10/2020 : controls if it's a reccurent user in order to apply a discout, 0 else
-     */
-
-    public void processExitingVehicle() {
-        try{
-            String vehicleRegNumber = getVehichleRegNumber();
-            Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
-            ParkingSpot parkingSpot = ticket.getParkingSpot();
-            this.discountProcess(parkingSpot.getParkingType(), ticket, false, ticketDAO.isRecurringUser(vehicleRegNumber));
-            Date outTime = new Date();
-            ticket.setOutTime(outTime);
-            fareCalculatorService.calculateFare(ticket);
-            if(ticketDAO.updateTicket(ticket)) {
-                parkingSpot.setAvailable(true);
-                parkingSpotDAO.updateParking(parkingSpot);
-                System.out.println("Please pay the parking fare:" + ticket.getPrice());
-                System.out.println("Recorded out-time for vehicle number:" + ticket.getVehicleRegNumber() + " is:" + outTime);
-            }else{
-                System.out.println("Unable to update ticket information. Error occurred");
-            }
-        }catch(Exception e){
-            logger.error("Unable to process exiting vehicle",e);
-        }
-    }
+  }
 }
